fc5a44330040fc0808af658f84d198974cd44413
Asadtop4ik <asadbek.backend@gmail.com>
2024-11-30 15:13:37 +0500

    questions

--- answers/urls.py
+++ answers/urls.py
@@ -1,9 +1,8 @@
-# from django.urls import path
-#
-# from .views import QuestionListView, UserResponseView, ResultView
-#
-# urlpatterns = [
-#     path('questions/', QuestionListView.as_view(), name='questions'),
-#     path('responses/', UserResponseView.as_view(), name='responses'),
-#     path('result/', ResultView.as_view(), name='result'),
-# ]
+from rest_framework.routers import DefaultRouter
+from .views import QuestionViewSet, AnswerViewSet
+
+router = DefaultRouter()
+router.register('questions', QuestionViewSet, basename='questions')  # Endpoint for questions
+router.register('answers', AnswerViewSet, basename='answers')  # Endpoint for answers
+
+urlpatterns = router.urls
