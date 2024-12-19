ef90f4249ad892144c01a18231de19faa11de2d7
Asadtop4ik <asadbek.backend@gmail.com>
2024-12-19 11:00:55 +0500

    new

--- custom_auth/models.py
+++ custom_auth/models.py
@@ -41,7 +41,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name', 'last_name', 'birth_date']
+    REQUIRED_FIELDS = ['first_name', 'last_name']
 
     def __str__(self):
         return self.email
