2830dc08b93413366bea6c97bffc388ad7d5d0f0
Asadtop4ik <asadbek.backend@gmail.com>
2024-12-25 11:55:14 +0500

    small wins for each user

--- answers/views/misc.py
+++ answers/views/misc.py
@@ -1,9 +1,13 @@
 from rest_framework import viewsets, permissions
+from rest_framework.exceptions import NotFound
+from rest_framework.response import Response
 from answers.models import SmallWin
 from answers.serializers import SmallWinSerializer
 from django.utils import timezone
 from datetime import timedelta
 from drf_spectacular.utils import extend_schema, OpenApiParameter
+from rest_framework.decorators import action
+
 class SmallWinViewSet(viewsets.ModelViewSet):
     queryset = SmallWin.objects.all()
     serializer_class = SmallWinSerializer
@@ -39,3 +43,14 @@ class SmallWinViewSet(viewsets.ModelViewSet):
         if period:
             self.queryset = self.get_period_queryset(period)
         return super().list(request, *args, **kwargs)
+
+    @action(detail=False, methods=['get'], url_path='user-small-win/(?P<user_id>[^/.]+)')
+    def user_has_small_win(self, request, user_id=None):
+        try:
+            user_small_wins = SmallWin.objects.filter(user_id=user_id)
+        except SmallWin.DoesNotExist:
+            raise NotFound({"detail": "No data found for this user."})
+
+
+        serializer = self.get_serializer(user_small_wins, many=True)
+        return Response(serializer.data)
